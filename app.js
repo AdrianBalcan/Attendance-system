327ea206f51536bc5d72b094e734793c8277bd3a
AdrianBalcan <contact@adrianbalcan.com>
2016-11-06 21:25:37 +0200

    db fix

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var config = {
     user: 'attendance', //env var: PGUSER
     database: 'attendance_dev', //env var: PGDATABASE
     password: 'attzog', //env var: PGPASSWORD
-    host: 'server.zog.ro', // Server hosting the postgres database
+    host: 'pontaj02.zog.ro', // Server hosting the postgres database
     port: 5432, //env var: PGPORT
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
